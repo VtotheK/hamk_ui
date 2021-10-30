67c23378bf5e22ef394de56af533ac149805490a
VtotheK <ville.keituri1@gmail.com>
2021-10-30 22:53:07 +0300

    calculator done

--- tehtava1/Laskin/Laskin/MainWindow.xaml.cs
+++ tehtava1/Laskin/Laskin/MainWindow.xaml.cs
@@ -20,14 +20,83 @@ namespace Laskin
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string formula;
+        private TextBox formulaField;
+        private bool calculated = false;
         public MainWindow()
         {
             InitializeComponent();
+            formulaField = FormulaField;
         }
 
         private void CalculatorInput(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine(sender.ToString());
+            if (calculated)
+            {
+                formula = "";
+                formulaField.Text = "";
+                calculated = false;
+            }
+            string input = ((Button)sender).Tag.ToString();
+            double eval;
+            switch (input)
+            {
+                case "=":
+                    try
+                    {
+                        eval = Evaluate(formula);
+                    }
+                    catch (ArgumentException r)
+                    {
+                        formulaField.Text = "Invalid expression";
+                        return;
+                    }
+                    formulaField.Text = eval.ToString();
+                    calculated = true;
+                    return;
+                case "C":
+                    formula = "";
+                    break;
+                case "CE":
+                    if (formula != null)
+                    {
+                        int i = formula.Length - 1;
+                        while (i >= 0 && Char.IsDigit(formula[i]))
+                        {
+                            formula = formula.Remove(i--, 1);
+                        }
+                    }
+                    break;
+                case "DEL":
+                    if (formula.Length > 0)
+                    {
+                        formula = formula.Remove(formula.Length - 1, 1);
+                    }
+                    break;
+                default:
+                    formula += input;
+                    break;
+            }
+            formulaField.Text = formula;
         }
+        private double Evaluate(string expression)
+        {
+            try
+            {
+                System.Data.DataTable table = new System.Data.DataTable();
+                table.Columns.Add("expression", string.Empty.GetType(), expression);
+                System.Data.DataRow row = table.NewRow();
+                table.Rows.Add(row);
+                return double.Parse((string)row["expression"]);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException();
+            }
+        }
+
     }
+
 }
+
+
