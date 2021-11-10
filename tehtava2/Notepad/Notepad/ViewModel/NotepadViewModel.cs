86bb425cb6cba5fcab9387825da5533f548c08fe
VtotheK <ville.keituri1@gmail.com>
2021-11-10 23:04:13 +0200

    removed useless edit viewmodel

--- tehtava2/Notepad/Notepad/ViewModel/NotepadViewModel.cs
+++ tehtava2/Notepad/Notepad/ViewModel/NotepadViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Notepad.View;
+﻿using System.Windows.Input;
+using Notepad.View;
 namespace Notepad.ViewModel
 {
     class NotepadViewModel
@@ -6,14 +7,12 @@ namespace Notepad.ViewModel
         private RelayCommand _formatMenuShow;
 
         private FileMenuViewModel _fileMenu;
-        private EditViewModel _editMenu;
         private FormatViewModel _formatMenu = new FormatViewModel();
         private MainWindow _mainWindow;
         public NotepadViewModel(MainWindow mw)
         {
             _mainWindow = mw;
             _fileMenu = new FileMenuViewModel(this);
-            _editMenu = new EditViewModel(mw.Notepad_textbox);
             FormatMenuShow = new RelayCommand(cFormatMenuShow);
         }
         public NotepadViewModel()
@@ -22,7 +21,6 @@ namespace Notepad.ViewModel
         public FileMenuViewModel FileMenu { get => _fileMenu; set => _fileMenu = value; }
         public FormatViewModel FormatMenu { get => _formatMenu; set => _formatMenu = value; }
         private MainWindow MainWindow { get => _mainWindow; }
-        public EditViewModel EditMenu { get => _editMenu; set => _editMenu = value; }
         public RelayCommand FormatMenuShow { get => _formatMenuShow; set => _formatMenuShow = value; }
 
         public string NotepadTextFieldContentGet()
