3c69d1fecd33f9c899261c794d022004c82673b3
VtotheK <ville.keituri1@gmail.com>
2021-11-14 00:17:08 +0200

    works

--- tehtava3/olympiarenkaat/Olympiarenkaat/Olympiarenkaat/MainWindow.xaml.cs
+++ tehtava3/olympiarenkaat/Olympiarenkaat/Olympiarenkaat/MainWindow.xaml.cs
@@ -28,21 +28,16 @@ namespace Olympiarenkaat
             InitializeComponent();
         }
 
-
         private void CreateOlympicRings(object sender, RoutedEventArgs e)
         {
-            if (CustomCanvasClass.IsChecked == true)
-            {
-                CreateOlympicRingsEllipses(true);
-            }
-            else
-            {
-                CreateOlympicRingsEllipses(false);
-            }
+            CreateOlympicRingsEllipses();
         }
-        private void CreateOlympicRingsEllipses(bool customClass)
+        private void CreateOlympicRingsEllipses()
         {
-            ClearRings(customClass);
+            foreach (var ring in rings)
+            {
+                OlympicCanvas.Children.Remove(ring);
+            }
             int ringHeight = 120;
             int ringWidth = 120;
             double x = (OlympicCanvas.ActualWidth / 2.0) - (1.6 * ringWidth);
@@ -54,22 +49,10 @@ namespace Olympiarenkaat
                 ring.Width = ringWidth;
                 ring.Stroke = ringcolors[i];
                 ring.StrokeThickness = 10;
-                if (customClass)
-                {
-                    CustomCanvas.Children.Add(ring);
-                    CustomCanvas.X = x;
-                    CustomCanvas.Y = y;
-                    Canvas.SetLeft(ring, x);
-                    Canvas.SetTop(ring, y);
-                    CustomCanvas.Rings.Add(ring);
-                }
-                else
-                {
-                    OlympicCanvas.Children.Add(ring);
-                    Canvas.SetLeft(ring, x);
-                    Canvas.SetTop(ring, y);
-                    rings.Add(ring);
-                }
+                OlympicCanvas.Children.Add(ring);
+                Canvas.SetLeft(ring, x);
+                Canvas.SetTop(ring, y);
+                rings.Add(ring);
                 if (i % 2 == 0)
                 {
                     x += ringWidth / 1.8;
@@ -85,41 +68,31 @@ namespace Olympiarenkaat
 
         private void ExplosionAnimation(object sender, RoutedEventArgs e)
         {
-            if (rings.Count() <= 0)
+            CustomCanvas.AddX += 20;
+            CustomCanvas.AddY += 20;
+            if (rings.Count() > 0)
             {
-                return;
-            }
-            var rnd = new Random();
-            for (int i = 0; i < rings.Count; ++i)
-            {
-                int randleft = rnd.Next(900, 1500);
-                int randtop = rnd.Next(900, 1500);
-                int dirX = rnd.Next(1, 3) > 1 ? -1 : 1;
-                int dirY = rnd.Next(1, 3) > 1 ? -1 : 1;
-                double left = Canvas.GetLeft(rings[i]);
-                double top = Canvas.GetTop(rings[i]);
-                DoubleAnimation xAn = new DoubleAnimation();
-                DoubleAnimation yAn = new DoubleAnimation();
-                xAn.From = left;
-                xAn.To = (left + randleft) * dirX;
-                yAn.From = top;
-                yAn.To = (top + randtop) * dirY;
-                xAn.Duration = new Duration(TimeSpan.Parse("0:0:5"));
-                yAn.Duration = new Duration(TimeSpan.Parse("0:0:5"));
-                rings[i].BeginAnimation(Canvas.LeftProperty, xAn);
-                rings[i].BeginAnimation(Canvas.TopProperty, yAn);
-            }
-        }
-
-        private void ClearRings(bool customClass)
-        {
-            foreach (var ring in rings)
-            {
-                OlympicCanvas.Children.Remove(ring);
-            }
-            foreach (var ring in CustomCanvas.Rings)
-            {
-                CustomCanvas.Children.Remove(ring);
+                var rnd = new Random();
+                for (int i = 0; i < rings.Count; ++i)
+                {
+                    int randleft = rnd.Next(900, 1500);
+                    int randtop = rnd.Next(900, 1500);
+                    int opac = 1;
+                    int dirX = rnd.Next(1, 3) > 1 ? -1 : 1;
+                    int dirY = rnd.Next(1, 3) > 1 ? -1 : 1;
+                    double left = Canvas.GetLeft(rings[i]);
+                    double top = Canvas.GetTop(rings[i]);
+                    DoubleAnimation xAn = new DoubleAnimation();
+                    DoubleAnimation yAn = new DoubleAnimation();
+                    xAn.From = left;
+                    xAn.To = (left + randleft) * dirX;
+                    yAn.From = top;
+                    yAn.To = (top + randtop) * dirY;
+                    xAn.Duration = new Duration(TimeSpan.Parse("0:0:5"));
+                    yAn.Duration = new Duration(TimeSpan.Parse("0:0:5"));
+                    rings[i].BeginAnimation(Canvas.LeftProperty, xAn);
+                    rings[i].BeginAnimation(Canvas.TopProperty, yAn);
+                }
             }
         }
     }
--- tehtava3/olympiarenkaat/Olympiarenkaat/Olympiarenkaat/MyCanvas.cs
+++ tehtava3/olympiarenkaat/Olympiarenkaat/Olympiarenkaat/MyCanvas.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -11,25 +12,40 @@ using System.Windows.Shapes;
 
 namespace Olympiarenkaat
 {
-    public class MyCanvas : Canvas 
+    public class MyCanvas : Canvas
     {
-        List<Ellipse> rings = new List<Ellipse>();
-        double x;
-        double y;
+        private SolidColorBrush[] ringcolors = { Brushes.Blue, Brushes.Yellow, Brushes.Black, Brushes.Green, Brushes.Red };
+        double addY = 0;
+        double addX = 0;
+        int[] xDirs = {-1, 0, 0, 2, 1 };
+        int[] yDirs = { 1, 1, 2, 2, 1 };
 
-        public double X { get => x; set => x = value; }
-        public double Y { get => y; set => y = value; }
-        public List<Ellipse> Rings { get => rings; set => rings = value; }
+        public double AddX { get => addX; set { addX = value; InvalidateVisual(); } }
+        public double AddY { get => addY; set { addY = value; InvalidateVisual(); } }
 
         protected override void OnRender(DrawingContext dc)
         {
-            foreach (var ring in Rings)
+            int ringHeight = 120;
+            int ringWidth = 120;
+            int thickness = 10;
+            double x = (this.ActualWidth / 2.0) - (1.8 * ringWidth);
+            double y = this.ActualHeight / 2;
+            for (int i = 0; i < ringcolors.Length; i++)
             {
-                double width = ring.Width;
-                double height = ring.Height;
-                Brush brush = ring.Stroke;
-                double thickness = ring.StrokeThickness;
-                dc.DrawEllipse(brush, new Pen(brush, thickness), new Point(X, Y), width + new Random().Next(10,200), height);
+                Pen pen = new Pen();
+                pen.Thickness = thickness;
+                pen.Brush = ringcolors[i];
+                if (i % 2 != 0)
+                {
+                    x += (ringWidth / 1.8) + (xDirs[i] * AddX);
+                    y += (ringHeight / 2.5) + (yDirs[i] * AddY);
+                }
+                else
+                {
+                    x += (ringWidth / 1.8) + (xDirs[i] * AddX);
+                    y -= (ringHeight / 2.5) + (yDirs[i] * AddY);
+                }
+                dc.DrawEllipse(null, pen, new Point(x , y), ringWidth/2, ringHeight/2);
             }
         }
     }
