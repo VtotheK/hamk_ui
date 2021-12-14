2e195ad23aabb3626c8e5a1bd75f1be2e81c286c
VtotheK <ville.keituri1@gmail.com>
2021-12-14 22:45:32 +0200

    color change works

--- tehtava4/Notepad/Notepad/View/Notepad.xaml.cs
+++ tehtava4/Notepad/Notepad/View/Notepad.xaml.cs
@@ -19,6 +19,7 @@ namespace Notepad
     {
         InkCanvas canvas; 
         NotepadViewModel vm;
+        Dictionary<string, System.Windows.Media.Color> colors = new Dictionary<string, System.Windows.Media.Color>();
         public MainWindow()
         {
             string lang = Properties.Settings.Default.AppLang;
@@ -33,9 +34,32 @@ namespace Notepad
 
         private void PopulateColorPanel()
         {
-            Type c = typeof(System.Windows.Media.Color);
+            Type c = typeof(System.Windows.Media.Colors);
             PropertyInfo[] props = c.GetProperties(BindingFlags.Static | BindingFlags.DeclaredOnly | BindingFlags.Public);
+            foreach (PropertyInfo info in props)
+            {
+                InkCanvasColors.Items.Add(CreateColorStackPanel(info.Name));
+            }
+        }
 
+        private StackPanel CreateColorStackPanel(string name)
+        {
+            StackPanel sp = new StackPanel();
+            sp.Orientation = Orientation.Horizontal;
+            System.Windows.Shapes.Rectangle rect = new System.Windows.Shapes.Rectangle();
+            rect.Width = 20;
+            rect.Height= 20;
+            System.Windows.Media.ColorConverter converter = new System.Windows.Media.ColorConverter();
+            System.Windows.Media.Color c = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString(name);
+            colors[name] = c;
+            rect.Fill = new SolidColorBrush(c);
+            rect.StrokeThickness = 2;
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = name;
+            textBlock.Name = name;
+            sp.Children.Add(rect);
+            sp.Children.Add(textBlock);
+            return sp;
         }
 
         private void Strokes_StrokesChanged(object sender, System.Windows.Ink.StrokeCollectionChangedEventArgs e)
@@ -69,6 +93,17 @@ namespace Notepad
             TextBox textBox = (TextBox)Notepad_textbox;
             ScrollViewer scrollBar = (ScrollViewer)sender;
             canvas.Margin = new Thickness(0, -scrollBar.VerticalOffset,0,0);
+        }
+
+        private void Colors_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var sp = (StackPanel)InkCanvasColors.SelectedValue;
+            var child = (TextBlock)sp.Children[1];
+            System.Windows.Media.Color selectedColor = new System.Windows.Media.Color();
+            if(colors.TryGetValue(child.Name,out selectedColor))
+            {
+                canvas.DefaultDrawingAttributes.Color = selectedColor;
+            }
         }
     }
 }
